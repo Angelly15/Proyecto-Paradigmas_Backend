6ce90ab20c8aea55c4b21e4eaff8e5472b758c73
Angelly15 <angelly.cortes.herrera@est.una.ac.cr>
2022-10-20 00:49:59 -0600

    Terminando clase persona

--- src/main/java/una/ac/cr/paradigmas/modelo/Persona.java
+++ src/main/java/una/ac/cr/paradigmas/modelo/Persona.java
@@ -4,10 +4,61 @@
  */
 package una.ac.cr.paradigmas.modelo;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Angelly Cortes
  */
-public class Persona {
+@Entity
+public class Persona implements Serializable {
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private long id;
+   private String Identificacion;
+   private String Nombre;
+
+    public Persona(long id, String Identificacion, String Nombre) {
+        this.id = id;
+        this.Identificacion = Identificacion;
+        this.Nombre = Nombre;
+    }
+
+    public Persona() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getIdentificacion() {
+        return Identificacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setIdentificacion(String Identificacion) {
+        this.Identificacion = Identificacion;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", Identificacion=" + Identificacion + ", Nombre=" + Nombre + '}';
+    }
     
+   
 }
