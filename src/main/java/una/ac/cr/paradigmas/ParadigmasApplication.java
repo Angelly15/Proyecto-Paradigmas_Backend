2145ca8263210f8694de6423e8d5e3e7ce77ada3
Angelly15 <angelly.cortes.herrera@est.una.ac.cr>
2022-10-19 23:51:42 -0600

    primer commit

---
+++ src/main/java/una/ac/cr/paradigmas/ParadigmasApplication.java
@@ -0,0 +1,13 @@
+package una.ac.cr.paradigmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadigmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadigmasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/una/ac/cr/paradigmas/ParadigmasApplicationTests.java
@@ -0,0 +1,13 @@
+package una.ac.cr.paradigmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParadigmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
