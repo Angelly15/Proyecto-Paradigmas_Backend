9b78ef014385117771ddc451b9c555c1e798eb39
Angelly15 <angelly.cortes.herrera@est.una.ac.cr>
2022-10-21 00:28:59 -0600

    Terminando PersonaAOP

---
+++ src/main/java/una/ac/cr/paradigmas/modelo/Log.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package una.ac.cr.paradigmas.modelo;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Angelly Cortes
+ */
+@Entity
+public class Log implements Serializable {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private String Metodo;
+    private LocalDateTime Fecha;           
+
+    public void setMetodo(String name) {
+      Metodo=name;
+    }
+
+    public void setFecha(LocalDateTime now) {
+        Fecha=now;
+    }
+    
+}
+
